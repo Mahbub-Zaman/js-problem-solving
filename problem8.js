f9ff3df3e4ebf6234c9fdc244dd6e3aed869593d
Mahbub-Zaman <mahbubzamanshuvo@gmail.com>
2026-01-08 14:15:59 +0600

    first commit

---
+++ problem8.js
@@ -0,0 +1,14 @@
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word =
+      words[i][0].toUpperCase() + words[i].slice(1);
+    result.push(word);
+  }
+  return result.join(" ");
+}
+
+console.log(capitalizeWords("hello world"));
